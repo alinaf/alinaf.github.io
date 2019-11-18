f4f4d707fc7546367a5dacc7b8e87e309d393224
Skylar MacMillan <smacmill@usc.edu>
2019-11-18 10:36:43 -0800

    timer still unsynced

--- js/client.js
+++ js/client.js
@@ -1,61 +1,62 @@
 var Client = {};
 Client.socket = io.connect();
 
-Client.sendTest = function(){
+Client.sendTest = function () {
     console.log("test sent");
     Client.socket.emit('test');
 };
 
-Client.askNewPlayer = function(){
+Client.askNewPlayer = function () {
     Client.socket.emit('newplayer');
 };
 
-Client.startGame = function(tileBag){
-    console.log(1);
+Client.startGame = function (tileBag) {
     Client.socket.emit('start', tileBag);
 };
 
 Client.newTile = function () {
-    console.log('testing 750 pm')
     Client.socket.emit('newtile');
 };
 
-Client.sendClick = function(x,y){
-  Client.socket.emit('click',{x:x,y:y});
+Client.sendClick = function (x, y) {
+    Client.socket.emit('click', { x: x, y: y });
 };
 
-Client.submitWord = function(word, score){
-  Client.socket.emit('submit',{word: word, score: score});
+Client.submitWord = function (word, score, letterBagTiles, total, dimensions) {
+    Client.socket.emit('submit', { word: word, score: score, tiles: letterBagTiles, total: total, dimensions: dimensions });
 };
 
-Client.socket.on('newtile', function() {
-    console.log('gonna make a new tile')
+Client.socket.on('newtile', function () {
     Game.newTile();
 });
 
-Client.socket.on('newplayer',function(data){
-    Game.addNewPlayer(data.id,data.x,data.y);
+Client.socket.on('newplayer', function (data) {
+    Game.addNewPlayer(data.id, data.x, data.y);
 });
 
 
-Client.socket.on('print',function(data){
-   Game.print(data);
+Client.socket.on('print', function (data) {
+    Game.print(data);
 });
 
-Client.socket.on('tilebag',function(data){
-   Game.setTileBag(data);
+Client.socket.on('tilebag', function (data) {
+    Game.setTileBag(data);
 });
 
-Client.socket.on('allplayers',function(data){
-    for(var i = 0; i < data.length; i++){
-        Game.addNewPlayer(data[i].id,data[i].x,data[i].y);
+Client.socket.on('startTimer', function () {
+    Game.startTimer();
+});
+
+Client.socket.on('allplayers', function (data) {
+    for (var i = 0; i < data.length; i++) {
+        Game.addNewPlayer(data[i].id, data[i].x, data[i].y);
     }
 
-    Client.socket.on('move',function(data){
-        Game.movePlayer(data.id,data.x,data.y);
+    Client.socket.on('move', function (data) {
+        Game.movePlayer(data.id, data.x, data.y);
     });
 
-    Client.socket.on('remove',function(id){
+    Client.socket.on('remove', function (id) {
         Game.removePlayer(id);
     });
 });
--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 const playerMap = {};
 var Game = {};
 var name = "";
+var gameStarted = false;
 
 Game.addNewPlayer = function (id, x, y) {
     console.log('add player')
@@ -10,25 +11,51 @@ Game.removePlayer = function (id) {
     console.log('remove');
 };
 
+Game.startTimer = function () {
+    gameStarted = true;
+};
+
 Game.print = function (data) {
-    var displayName = name ? name : "Player " + data.id;
+    // var displayName = name ? name : "Player " + data.id;
+    var displayName = "They";
     var points = data.score == 1 ? " point!" : " points!";
     bonusText.setText(displayName + " played " + data.word + " for " + data.score + points);
+    otherScoreText.setText("them: " + data.total);
     addWord(context, data.word, !p1);
+    letterBagTiles = data.tiles;
+    draw();
+    if (data.dimensions) {
+        var arr = data.dimensions.left ? leftWords : rightWords;
+        var wordToDestroy = arr[data.dimensions.index];
+        for (i = 0; i < wordToDestroy.length; i++) {
+            wordToDestroy[i].square.destroy();
+            wordToDestroy[i].text.destroy();
+        }
+    }
 };
 
 Game.setTileBag = function (data) {
     start.destroy();
     letterBag = data;
-    addTiles();
+    line.visible = true;
+    if (p1) drawTile(true);
+    var youWidth = p1 ? w / 4 : 3 * w / 4;
+    var themWidth = p1 ? 3 * w / 4 : w / 4;
+
+
+    scoreText = context.add.text(youWidth, h - 100, "you: 0", {
+        font: "40px Karla",
+        fill: '#000000'
+    });
+    otherScoreText = context.add.text(themWidth, h - 100, "them: 0", {
+        font: "40px Karla",
+        fill: '#000000'
+    });
 };
 
 Game.newTile = function () {
     drawTile(false);
-    console.log('hi sky')
 };
-
-
 
 // dimensions
 const w = window.innerWidth;
@@ -36,26 +63,30 @@ const h = window.innerHeight;
 // const topThird = h/3;
 
 var p1 = false;
+var leftWords = [];
+var leftWordCounter = 0;
+var rightWords = [];
+var rightWordCounter = 0;
+
 var letterBag;
 const brightBlue = "#459ac4";
 var currentWord = "";
-var wordStrings = [];
 var letterMap = new Map();
 var squareToTextBox = new Map();
 var currentSquares = [];
 var squareToLocation = new Map();
+var squareToIndex = new Map();
+var letterBagTiles = new Array(); // array of letters
 
-var pos = 0; // no queue in JS apparently so just use this
+var pos = 0; // no queue in JS apparently so just use this, position in letterbag
 var plurals = new Set(); // doesn't actually work because each word needs its own memory
 // put "s" in (hacky), lots of complexities i'm skimming over
 const dictString = readTextFile("assets/words_alpha.txt").split(/\s+/);
 
 const validWords = new Set(dictString);
 
-console.log(validWords);
-
-var lpos = 200; // left current height
-var rpos = 200; // right current height
+var lpos = 225; // left current height
+var rpos = 225; // right current height
 var left = true;
 var config = {
     type: Phaser.WEBGL,
@@ -70,6 +101,10 @@ var config = {
     }
 };
 
+// timer stuff
+var timerText;
+var timedEvent;
+
 var context;
 var game = new Phaser.Game(config);
 var bagSquares = [];
@@ -77,11 +112,15 @@ var currentWordText;
 var camera;
 var score = 0;
 var scoreText;
+var otherScoreText;
 var showingMessage = false;
 var bonusText;
 var instructions;
 var instructionsShowing = false;
 var start;
+var previousTiles = [];
+var line;
+var divider;
 
 function preload() {
     this.load.image('square', 'assets/square.png');
@@ -94,9 +133,23 @@ function preload() {
 }
 
 function create() {
-    // waste another hour on this line later please
-    var line = this.add.line(w / 2, h / 2 + 50, 0, 0, 0, 3 * h / 4, 0xE6AC8E);
+
+    this.initialTime = 150;
+    timerText = this.add.text(w / 2, h - 50, 'Timer: ' + formatTime(this.initialTime), {
+        font: "20px Karla",
+        fill: '#142E28'
+    });
+    
+
+    // Each 1000 ms call onEvent
+    timedEvent = this.time.addEvent({ delay: 1000, callback: onEvent, callbackScope: this, loop: true });
+
+    line = this.add.line(w / 2, h / 2 + 50, 0, 0, 0, 3 * h / 4, 0xE6AC8E);
     line.setLineWidth(5);
+    line.visible = false;
+    line = this.add.line(w / 2, 165, 0, 0, w, 0, 0xE6AC8E);
+    line.setLineWidth(5);
+    //line.visible = false; 
     bonusText = this.add.text(w / 2, 15, "", {
         font: "20px Karla",
         fill: '#000000'
@@ -107,24 +160,44 @@ function create() {
     camera = this.cameras.main;
     currentWordText = this.add.text(w / 2, h - 200, "", {
         font: "70px Merriweather",
-        fill: '#054f4a'
+        fill: '#142E28'
     });
     currentWordText.setOrigin(0.5);
-    scoreText = this.add.text(w / 2, h - 100, "score: 0", {
-        font: "55px Karla",
-        fill: '#000000'
-    });
-    scoreText.setOrigin(0.5);
+
     this.input.keyboard.on('keydown_ENTER', submitWord);
     this.input.keyboard.on('keydown_BACKSPACE', deleteLetter);
-    this.input.keyboard.on('keydown_SPACE', function () { return drawTile(true); });
-    start = this.add.sprite(700, 300, 'submit');
+    this.input.keyboard.on('keydown_SPACE', function () {
+        drawTile(true);
+    });
+    start = this.add.sprite(w / 2, h / 2, 'submit');
+    start.setOrigin(0.5);
     start.setInteractive();
     start.on('pointerup', function (pointer) {
         Client.startGame(getTileBag())
+
         p1 = true; // started game
         this.destroy();
     });
+
+}
+
+function formatTime(seconds) {
+    // Minutes
+    var minutes = Math.floor(seconds / 60);
+    // Seconds
+    var partInSeconds = seconds % 60;
+    // Adds left zeros to seconds
+    partInSeconds = partInSeconds.toString().padStart(2, '0');
+    // Returns formated time
+    return `${minutes}:${partInSeconds}`;
+}
+
+function onEvent() {
+    if (p1 || gameStarted) {
+        this.initialTime -= 1; // One second
+        timerText.setText('Timer: ' + formatTime(this.initialTime));
+    }
+    
 }
 
 function getTileBag() {
@@ -140,40 +213,48 @@ function getTileBag() {
     return Phaser.Utils.Array.Shuffle(unshuffledLetterBag);
 }
 
-function addTiles() {
-    // make grid
+function draw() {
+    if (previousTiles.length != 0) {
+        // delete from map
+        for (i = 0; i < previousTiles.length; i++) {
+            previousTiles[i].letter.destroy();
+            previousTiles[i].square.destroy();
+        }
+        previousTiles = [];
+    }
+
     var width = 0;
-    for (i = 0; i < 15; i++) { // change this!!
-        width += 85;
-        const square = context.add.sprite(width, 75, 'tile');
+    for (i = 0; i < letterBagTiles.length; i++) {
+        width += 105;
+        const square = context.add.sprite(width, 85, 'tile');
         square.tint = 0xE6AC8E;
-        text = context.add.text(square.x - 18, square.y - 33, letterBag[i], {
-            font: "60px Merriweather",
+        text = context.add.text(square.x - 18, square.y - 33, letterBagTiles[i], {
+            font: "70px Merriweather",
             fill: '#FFFFFF'
         });
         square.setInteractive();
-        text.visible = false;
-        square.visible = false;
-        letterMap.set(square, letterBag[i]);
+        letterMap.set(square, letterBagTiles[i]);
         squareToTextBox.set(square, text);
+        squareToIndex.set(square, i);
+        previousTiles.push({ letter: text, square: square });
         square.on('pointerover', function (pointer) {
             this.setTint(0xE5381B);
         });
         square.on('pointerout', function (pointer) {
-            this.setTint(0xE6AC8E);
+            if (!currentSquares.includes(this)) {
+                this.setTint(0xE6AC8E);
+            }
         });
         square.on('pointerup', function (pointer) {
+            this.setTint(0xE5381B);
             updateString(this, letterMap.get(this));
         });
         bagSquares.push(square);
     }
-    console.log('ahhh');
-    drawTile(true);
 }
 
 async function submitWord() {
-    if (currentWord.length < 3) return;
-    console.log(currentWord);
+    if (currentWord.length < 4) return;
     const errorMessage = validWord(currentWord);
     if (errorMessage != "") {
         camera.shake(700, 0.003);
@@ -187,16 +268,11 @@ async function submitWord() {
     plurals.add(currentWord + "s");
     var bonus = calculateScore(currentWord);
     score += bonus;
-    Client.submitWord(currentWord, bonus);
     var points = bonus == 1 ? " point!" : " points!";
     bonusText.setText("You played " + currentWord + " for " + bonus + points);
     scoreText.setText("score: " + score);
-    //const dimensions = canRearrange();
-    //
+    const dimensions = canRearrange();
     addWord(context, currentWord, p1);
-    //}
-    currentWord = "";
-    currentWordText.setText(currentWord);
     var bonusImage;
     if (bonus > 10) {
         bonusImage = context.add.sprite(700, 300, 'excellent');
@@ -205,11 +281,23 @@ async function submitWord() {
         bonusImage = context.add.sprite(700, 300, 'nice');
         bonusImage.alpha = 0.9;
     }
+    var deleteIndices = [];
     for (i = 0; i < currentSquares.length; i++) {
+        if (squareToIndex.get(currentSquares[i]) != null) {
+            deleteIndices.push(squareToIndex.get(currentSquares[i]));
+        }
         squareToTextBox.get(currentSquares[i]).destroy();
         currentSquares[i].destroy();
     }
+    deleteIndices.sort(function (a, b) { return a - b; });
+    for (var i = deleteIndices.length - 1; i >= 0; i--) {
+        letterBagTiles.splice(deleteIndices[i], 1);
+    }
+    Client.submitWord(currentWord, bonus, letterBagTiles, score, dimensions);
+    currentWord = "";
+    currentWordText.setText(currentWord);
     currentSquares = [];
+    draw();
     if (bonusImage) {
         await sleep(1000);
         bonusImage.destroy();
@@ -260,9 +348,8 @@ function deleteLetter() {
     if (!currentWord.length) return;
     currentWord = currentWord.slice(0, -1);
     currentWordText.setText(currentWord);
-    console.log(currentSquares);
+    currentSquares[currentSquares.length - 1].setTint(0xE6AC8E);
     currentSquares.pop();
-    console.log(currentSquares);
 }
 
 function updateString(square, letter) {
@@ -278,82 +365,56 @@ function updateString(square, letter) {
 }
 
 function drawTile(isSpacebar) {
-    if (instructionsShowing) instructions.destroy();
-    if (pos == letterBag.length) {
-        if (!showingMessage) {
-            const ranout = context.add.sprite(700, 300, 'ranout');
-            ranout.setInteractive();
-            showingMessage = true;
-            ranout.on('pointerup', function (pointer) {
-                this.destroy();
-                showingMessage = false;
-            });
-        }
-        return;
-    }
-    bagSquares[pos].visible = true;
-    squareToTextBox.get(bagSquares[pos]).visible = true;
+    // if (instructionsShowing) instructions.destroy();
+    // if (pos == letterBag.length) {
+    //     if (!showingMessage) {
+    //         const ranout = context.add.sprite(700, 300, 'ranout');
+    //         ranout.setInteractive();
+    //         showingMessage = true;
+    //         ranout.on('pointerup', function (pointer) {
+    //             this.destroy();
+    //             showingMessage = false;
+    //         });
+    //     }
+    //     return;
+    // }
+    //bagSquares[pos].visible = true;
+    // squareToTextBox.get(bagSquares[pos]).visible = true;
+    // pos++;
+    letterBagTiles.push(letterBag[pos]);
+    draw();
     pos++;
     if (isSpacebar) {
         Client.newTile();
     }
-    
 }
 
-// function canRearrange(word) {
-// 	for (i = 0; i < currentSquares.length; i++) {
-// 		if (squareToLocation.has(currentSquares[i])) {
-// 			const loc = squareToLocation.get(currentSquares[i]);
-// 			return {
-// 				width: loc.width,
-// 				height: loc.height
-// 			};
-// 		}
-// 	}
-// 	return false;
-// }
-
-// function rearrange(context, word, width, height) {
-// 	const startWidth = width;
-// 	for (i = 0; i < word.length; i++) {
-// 		width += 75;
-
-// 		const square = context.add.sprite(width, height, 'square');
-// 		squareToLocation.set(square, {
-// 			height: currHeight,
-// 			width: startWidth,
-// 			length: word.length
-// 		});
-// 		square.tint = 0xE6AC8E;
-// 		square.on('pointerover', function(pointer) {
-// 			this.setTint(0xE5381B);
-// 		});
-// 		square.on('pointerout', function(pointer) {
-// 			this.setTint(0xE6AC8E);
-// 		});
-// 		square.on('pointerup', function(pointer) {
-// 			updateString(this, letterMap.get(this));
-// 		});
-// 		text = context.add.text(square.x - 18, square.y - 33, word[i], {
-// 			font: "50px Merriweather",
-// 			fill: '#FFFFFF'
-// 		});
-// 		square.setInteractive();
-// 		letterMap.set(square, word[i]);
-// 		squareToTextBox.set(square, text);
-// 	}
-// }
+function canRearrange(word) {
+    for (i = 0; i < currentSquares.length; i++) {
+        if (squareToLocation.has(currentSquares[i])) {
+            const loc = squareToLocation.get(currentSquares[i]);
+            return {
+                left: loc.left,
+                index: loc.index,
+                length: loc.length
+            };
+        }
+    }
+    return false;
+}
 
 function addWord(context, word, left) {
     var width = left ? 0 : w / 2;
     var currHeight = left ? lpos : rpos;
     const startWidth = width;
+    var squares = [];
+    var index = left ? leftWordCounter : rightWordCounter;
     for (i = 0; i < word.length; i++) {
         width += 75;
         const square = context.add.sprite(width, currHeight, 'square');
         squareToLocation.set(square, {
-            height: currHeight,
-            width: startWidth,
+            left: left,
+            index: index,
             length: word.length
         });
         square.tint = 0xE6AC8E;
@@ -361,9 +422,12 @@ function addWord(context, word, left) {
             this.setTint(0xE5381B);
         });
         square.on('pointerout', function (pointer) {
-            this.setTint(0xE6AC8E);
+            if (!currentSquares.includes(this)) {
+                this.setTint(0xE6AC8E);
+            }
         });
         square.on('pointerup', function (pointer) {
+            this.setTint(0xE5381B);
             updateString(this, letterMap.get(this));
         });
         text = context.add.text(square.x - 18, square.y - 33, word[i], {
@@ -373,15 +437,21 @@ function addWord(context, word, left) {
         square.setInteractive();
         letterMap.set(square, word[i]);
         squareToTextBox.set(square, text);
+        squares.push({ square: square, text: text });
     }
-    if (left) lpos += 85;
-    if (!left) rpos += 85;
+    if (left) {
+        lpos += 85;
+        leftWords[leftWordCounter] = squares;
+        leftWordCounter++;
+    }
+    else {
+        rpos += 85;
+        rightWords[rightWordCounter] = squares;
+        rightWordCounter++;
+    }
 }
 
 function calculateScore(word) {
-    if (word.length == 3) {
-        return 1;
-    }
     if (word.length < 7) return word.length;
     return word.length * 2;
 }
